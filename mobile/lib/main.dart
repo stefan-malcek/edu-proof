6f04fdd30d318cf10745d02fdb43cfdfeed33b52
Štefan Malček <stefan.malcek1@gmail.com>
2024-04-28 20:31:24 +0200

    Setup default view.

--- mobile/lib/main.dart
+++ mobile/lib/main.dart
@@ -1,4 +1,10 @@
+import 'dart:convert';
+
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_appauth/flutter_appauth.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:jaguar_jwt/jaguar_jwt.dart';
 
 void main() {
   runApp(const MyApp());
@@ -13,21 +19,6 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // TRY THIS: Try running your application with "flutter run". You'll see
-        // the application has a purple toolbar. Then, without quitting the app,
-        // try changing the seedColor in the colorScheme below to Colors.green
-        // and then invoke "hot reload" (save your changes or press the "hot
-        // reload" button in a Flutter-supported IDE, or press "r" if you used
-        // the command line to start the app).
-        //
-        // Notice that the counter didn't reset back to zero; the application
-        // state is not lost during the reload. To reset the state, use hot
-        // restart instead.
-        //
-        // This works for code too, not just values: Most code changes can be
-        // tested with just a hot reload.
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
@@ -55,71 +46,72 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+  final appAuth = FlutterAppAuth();
 
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
+  Object? _idToken;
+
+  Future<void> _callAzureDefaultView() async {
+    try {
+      final AuthorizationTokenRequest request = AuthorizationTokenRequest(
+        'c00aa7fb-c8d3-4e49-b29a-05f454896e0a',
+        'com.example.eduproof://oauthredirect',
+        discoveryUrl:
+            'https://eduproofofconcept.b2clogin.com/eduproofofconcept.onmicrosoft.com/v2.0/.well-known/openid-configuration?p=B2C_1_susi',
+        scopes: ['openid', 'profile', 'email', 'offline_access'],
+        promptValues: ['login'],
+      );
+      final result = await appAuth.authorizeAndExchangeCode(request);
+      setState(() {
+        _idToken = _decodeToken(result?.idToken);
+        print(_formatJson(_idToken));
+      });
+    } catch (e) {
+      print(e);
+    }
+  }
+
+  String? _decodeToken(String? token) {
+    if (token == null) {
+      return null;
+    }
+
+    final parts = token.split('.');
+    final payload = parts[1];
+    return B64urlEncRfc7515.decodeUtf8(payload);
+  }
+
+  String? _formatJson(Object? data) {
+    if (data == null) {
+      return null;
+    }
+    var encoder = const JsonEncoder.withIndent("     ");
+    return encoder.convert(data);
   }
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
       appBar: AppBar(
-        // TRY THIS: Try changing the color here to a specific color (to
-        // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
-        // change color while the other colors stay the same.
         backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
         title: Text(widget.title),
       ),
       body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
         child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          //
-          // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
-          // action in the IDE, or press "p" in the console), to see the
-          // wireframe for each widget.
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
+            ElevatedButton(
+              child: Text('Azure AD B2C Default View'),
+              onPressed: _callAzureDefaultView,
+            ),
             const Text(
-              'Stlačil tlačidlo toľkoto krát:',
+              'User data',
             ),
             Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
+              '$_idToken',
             ),
           ],
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
 }
